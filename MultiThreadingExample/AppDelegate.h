b9010b7d4ef04264afd1a5046b42d9a943a6d235
Ben-G <Benjamin.Encz@gmail.com>
2014-09-10 08:55:15 -0700

    negative example completed

---
+++ MultiThreadingExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiThreadingExample
+//
+//  Created by Benjamin Encz on 10/09/14.
+//  Copyright (c) 2014 MakeSchool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiThreadingExample/ViewController.m
@@ -0,0 +1,73 @@
+//
+//  ViewController.m
+//  MultiThreadingExample
+//
+//  Created by Benjamin Encz on 10/09/14.
+//  Copyright (c) 2014 MakeSchool. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *downloadStatusLabel;
+@property (strong, nonatomic) NSArray *strings;
+@property (weak, nonatomic) IBOutlet UILabel *displayedStringLabel;
+@property (assign, nonatomic) NSInteger currentStringIndex;
+
+@end
+
+@implementation ViewController
+
+#pragma mark - Init
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        self.strings = @[@"Test1", @"Test2", @"Test3"];
+    }
+    
+    return self;
+}
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    self.displayedStringLabel.text = self.strings[self.currentStringIndex];
+    
+    [self downloadData];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+#pragma mark - Memory Management
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Button Callbacks
+
+- (IBAction)showRandomTextPressed:(id)sender {
+    self.currentStringIndex++;
+    self.currentStringIndex = self.currentStringIndex >= [self.strings count] ? 0 : self.currentStringIndex;
+    self.displayedStringLabel.text = self.strings[self.currentStringIndex];
+}
+
+#pragma mark - Download Management
+
+- (void)downloadData {
+    for (int i = 0; i < 20; i++) {
+        sleep(1);
+        self.downloadStatusLabel.text = [NSString stringWithFormat:@"%d/%d", i+1, 20];
+    }
+}
+
+@end
